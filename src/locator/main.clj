9dbea4cae60b800fd0b1e4c2fd4cb13f308cc426
Alexander Juda <alexander.juda@icloud.com>
2019-03-02 18:08:26 +0100

    Add neighbor solution generation

--- src/locator/main.clj
+++ src/locator/main.clj
@@ -4,14 +4,23 @@
 (defn example-solution []
   [5 4 0 9 10 8 6 1 3 11 2 7])
 
-(defn -main []
-  (println "hello!"))
-
 
 (defn swap-positions [v i1 i2]
   (-> v
       (assoc i1 (nth v i2))
       (assoc i2 (nth v i1))))
+
+(defn random-ints-pair [n]
+  (let [i1 (rand-int n)
+        i2 (rand-int (dec n))]
+    (if (< i2 i1)
+      [i1 i2]
+      [i1 (inc i2)])))
+
+
+(defn gen-neighbor-solution [s]
+  (let [[i1 i2] (random-ints-pair (count s))]
+    (swap-positions s i1 i2)))
 
 (defn energy [s {:keys [n a b]}]
   (->> (for [i (range n)
@@ -23,6 +32,9 @@
                        (get-in b [(get s i) (get s j)]))))
                0)))
 
+(defn -main []
+  (println "hello!"))
+
 (comment
   (+ 1 2)
 
@@ -32,5 +44,7 @@
   (swap-positions (vec (range 10)) 2 4)
 
   (energy (example-solution) (problem/example-problem))
+
+  (gen-neighbor-solution [0 1 2 3])
 
   )
