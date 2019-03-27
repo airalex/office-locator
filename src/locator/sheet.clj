391bad4d05eb042ea6c6d174296980ff41b34a3c
Alexander Juda <alexander.juda@icloud.com>
2019-03-27 22:46:49 +0100

    Parse indexed dataframe into rectangular dataframe

--- src/locator/sheet.clj
+++ src/locator/sheet.clj
@@ -27,8 +27,26 @@
                                        scores)))
                    (apply concat)
                    (into {}))]
-    {:cells cells
+    {:indexed-cells cells
      :labels colleagues}))
+
+(defn transpose [rows]
+  (apply map vector rows))
+
+(defn indexed-df->rect-df [{:keys [indexed-cells
+                                   labels]}
+                           & {:keys [missing-fill]}]
+  (let [sorted-labels (sort labels)
+        vals-cols (reduce (fn [matrix-acc col-name]
+                            (conj (vec matrix-acc)
+                                  (reduce (fn [col-acc row-name]
+                                            (conj col-acc
+                                                  (get indexed-cells {:row row-name
+                                                                      :col col-name} missing-fill)))
+                                          [] sorted-labels)))
+                          [] sorted-labels)]
+    {:cell-vals (transpose vals-cols)
+     :labels sorted-labels}))
 
 (comment
   (println "ab")
@@ -36,10 +54,13 @@
   (-> (csv/read-csv "a,b,c\n1,2,3\n4,5,6")
       (csv-rows->maps))
 
+  (transpose [[1 2] [3 4]])
+
   (-> (let [path (io/resource "responses/Jak skutecznie miejsca (Responses) - TM_TM.csv")]
         (with-open [reader (io/reader path)]
-          (-> (csv/read-csv reader)
-              (parse-tm-tm-rows))))
+          (-> (take 10 (csv/read-csv reader))
+              (parse-tm-tm-rows)
+              (indexed-df->rect-df :missing-fill 0))))
       (clojure.pprint/pprint))
 
   )
